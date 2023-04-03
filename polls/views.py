7606b06290b29e43526c9f699f1281f34e87456d
takeYY <t0k6yist@gmail.com>
2023-04-03 21:50:44 +0900

    add: a polls route

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
-
 # Create your views here.
+from django.http import HttpResponse
+
+# from django.shortcuts import render
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
